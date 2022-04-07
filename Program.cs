2f392d284bd3414c5925b698284bb33dac8a52d4
Kacper Grzybkowski <kacperg@gmail.com>
2022-04-07 21:31:37 +0200

    pierwsze pliki

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ChallengeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine($"Hello World! {args[0]}");
+        }
+    }
+}
